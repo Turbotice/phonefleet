af4bef474ce35ab842944d85ac78732d3b0ea7fd
sperrard <stephane.perrard@espci.fr>
2025-12-14 23:46:53 +0100

    Implement check of last Gobannos files

--- phonefleet/server/check_gobannos_files.py
+++ phonefleet/server/check_gobannos_files.py
@@ -31,32 +31,18 @@ def last_modified(filelist):
 		stat['filename']=os.path.basename(filename)
 		stat['size']= os.path.getsize(filename)
 		stats.append(stat)
+	return stats
+
+def display_stat(stats):
 	for stat in stats:
-		for key in stat.keys():
+                print('')
+		for key in ['filename','tm','size']:
 			print(key,stat[key])
-	return stat
 
-def main():
-	network = 223
-	phone = 197
-	port = 8080
-	ip = f"192.168.{network}.{phone}"
-	url = f"http://{ip}:{port}"
-
-	a = urllib.request.urlopen(f"{url}/status").read()
-	if a==b'STOPPED':
-		a = urllib.request.urlopen(f"{url}/start").read()
-		pass
-	else:
-		pass
-		#print('Gobannos already running')
-	time.sleep(0.1)
-	a = urllib.request.urlopen(f"{url}/status").read()
-	s = time.asctime(time.gmtime())
-	print(s+", "+a.decode())
-
-def test_active():
-	filelist = list_recent_files()
-	last_modified(filelist)
-
-test_active()
+def test_last_data():
+        filelist = list_recent_files(Dt=3600)
+        stats = last_modified(filelist)
+        display_stat(stats)
+        
+if __name__=='__main__':
+        test_last_data()
