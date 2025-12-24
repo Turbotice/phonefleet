b402eff5298b9e84f12b81c5c68c0737b8814429
SP from FP4_00 <stephane.perrard@espci.fr>
2025-12-24 18:01:05 +0100

    to check local status, using the local phone ip adress (127.0.0.1), should work offline

---
+++ phonefleet/server/status.py
@@ -0,0 +1,19 @@
+import urllib.request
+import time
+
+
+import phonefleet.server.connect as connect
+
+def main():
+	network,phone = connect.get_ip()
+	port = 8080
+	ip = f"192.168.{network}.{phone}"
+	ip = "127.0.0.1"
+
+	print(ip)
+	url = f"http://{ip}:{port}"
+	a = urllib.request.urlopen(f"{url}/status").read()
+	print(a)
+
+if __name__=='__main__':
+        main()
