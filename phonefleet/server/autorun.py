23588d78217569c2750f07dd05f1d6b7585d3483
SP_FP3_00 <stephane.perrard@espci.fr>
2025-12-24 17:28:47 +0100

    script to autorun gobannos from the phone

--- phonefleet/server/autorun.py
+++ phonefleet/server/autorun.py
@@ -3,24 +3,31 @@ import time
 
 import phonefleet.server.connect as connect
 
+#autorun Gobannos for N minutes, add an arg parser object
 
-def main():
-        network,phone = connect.get_ip()
+def main(T=300):
+	network,phone = connect.get_ip()
 	port = 8080
 	ip = f"192.168.{network}.{phone}"
 	url = f"http://{ip}:{port}"
 
 	a = urllib.request.urlopen(f"{url}/status").read()
 	if a==b'STOPPED':
-		#a = urllib.request.urlopen(f"{url}/start").read()
+		a = urllib.request.urlopen(f"{url}/start").read()
 		pass
 	else:
-		pass
-		#print('Gobannos already running')
+		print('Gobannos already running')
+	time.sleep(0.1)
+	a = urllib.request.urlopen(f"{url}/status").read()
+	s = time.asctime(time.gmtime())
+	print(s+", "+a.decode())
+
+	time.sleep(T)
+	a = urllib.request.urlopen(f"{url}/stop").read()
 	time.sleep(0.1)
 	a = urllib.request.urlopen(f"{url}/status").read()
 	s = time.asctime(time.gmtime())
 	print(s+", "+a.decode())
 
 if __name__=='__main__':
-        main()
+	main()
