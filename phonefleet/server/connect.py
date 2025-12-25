cef70527186c95df679f5667a9334e4ec7070efb
sperrard <stephane.perrard@espci.fr>
2025-12-25 10:45:19 +0100

    return full ip rather than network,phone

--- phonefleet/server/connect.py
+++ phonefleet/server/connect.py
@@ -1,11 +1,30 @@
 import subprocess
 
-global network
-global phone
 global port
 port = 8080
 
 def get_ip(protocol='wlan'):
+        #possible protocols :
+        # wlan : connection through the Wifi
+        # self : use internal ip adress, should be more robust
+        
+        if protocol=='self':
+                ip = '127.0.0.1'
+        elif protocol=='wlan':
+                out = subprocess.run('ifconfig',capture_output=True)
+                ip = out.stdout.decode().split(protocol)[1].split('inet ')[1].split(' netmask')[0]
+        else:
+                print('protocol is not implemented, check phonefleet.server.connect')
+                ip = None
+        return ip
+#                numbers = ip.split('.')
+#                network = int(numbers[2])
+#                phone = int(numbers[3])
+#	return network,phone
+
+def get_local_ip(protocol='wlan'):
+        #possible protocols :
+        # wlan : connection through the Wifi
 	out = subprocess.run('ifconfig',capture_output=True)
 	ip = out.stdout.decode().split(protocol)[1].split('inet ')[1].split(' netmask')[0]
 	print(ip)
@@ -14,6 +33,6 @@ def get_ip(protocol='wlan'):
 	phone = int(numbers[3])
 	return network,phone
 
-network,phone = get_ip()
+#network,phone = get_ip()
 
 
