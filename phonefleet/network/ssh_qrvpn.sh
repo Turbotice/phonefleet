9669b181d25d87cf64492de809dc9f27c06628df
sperrard <stephane.perrard@espci.fr>
2026-08-14 12:50:17 +0200

    add port to handle both phone and computers

--- phonefleet/network/ssh_qrvpn.sh
+++ phonefleet/network/ssh_qrvpn.sh
@@ -1,6 +1,6 @@
 while read line; do echo $line; done < routing_table.txt
 echo $1
-while read line; do id=`echo $line | awk -F ' ' '{print $1}'`; if [[ $id == $1 ]]; then num=`echo $line | awk -F ' ' '{print $2}'`; ip=`echo $line | awk -F ' ' '{print $3}'`; fi; done < routing_table.txt
+while read line; do id=`echo $line | awk -F ' ' '{print $1}'`; if [[ $id == $1 ]]; then num=`echo $line | awk -F ' ' '{print $2}'`; ip=`echo $line | awk -F ' ' '{print $3}'`; port=`echo $line | awk -F ' ' '{print $4}'`; fi; done < routing_table.txt
 echo $num
 echo $ip
-ssh $num@$ip -p 8022
+ssh $num@$ip -p $port
