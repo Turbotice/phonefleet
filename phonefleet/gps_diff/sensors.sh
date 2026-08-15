6a8b19dfe44773fc61cd98d467920cc0c5ac6284
sperrard <stephane.perrard@espci.fr>
2026-08-15 09:51:49 +0200

    update

--- phonefleet/gps_diff/sensors.sh
+++ phonefleet/gps_diff/sensors.sh
@@ -1,3 +1,3 @@
 date >> ~/log/sensors_log.txt
-date "+%H:%M:%S.%N" >> ~/log/sensors_log.txt
+date "+%H:%M:%S.%N" >> ~/log/sensors_log_260815.txt
 termux-sensor -s linear_acceleration,mmc56,Rotation -d 100 -n 500 >> ~/log/sensors_log_260815.txt
