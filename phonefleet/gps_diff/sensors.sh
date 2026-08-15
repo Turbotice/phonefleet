1063ce93553ad4a8943ea93b10145fea05bb9877
sperrard <stephane.perrard@espci.fr>
2026-08-15 09:46:51 +0200

    increase duration of sensor use to compute trajectories

--- phonefleet/gps_diff/sensors.sh
+++ phonefleet/gps_diff/sensors.sh
@@ -1,3 +1,3 @@
 date >> ~/log/sensors_log.txt
 date "+%H:%M:%S.%N" >> ~/log/sensors_log.txt
-termux-sensor -s linear_acceleration,mmc56,Rotation -d 100 -n 20 >> ~/log/sensors_log.txt
+termux-sensor -s linear_acceleration,mmc56,Rotation -d 100 -n 500 >> ~/log/sensors_log_260815.txt
